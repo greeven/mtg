0901b42ea76270fb53194081e8c883a6c12fc723
greeven <greeven1@gmail.com>
2017-04-01 19:56:52 +0200

    Variabler Port für Heroku.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', function (req, res) {
   res.render('index.html')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+const PORT = process.env.PORT || 1337;
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '!')
 })
