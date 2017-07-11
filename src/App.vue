2fced4d4173f6479323ba5daf51acd2821fab75d
greeven <greeven1@gmail.com>
2017-07-12 01:07:28 +0200

    Added viewport.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
+    <!-- <img src="./assets/logo.png"> aadfsdf-->
 
 
     <b-navbar toggleable type="inverse" variant="inverse" fixed="top">
